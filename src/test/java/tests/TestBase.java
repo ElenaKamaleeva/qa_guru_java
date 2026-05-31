be992cc8b01c86fae177a463464f0b9ec936ccbd
Elena Kamaleeva <akinakaoin@mail.ru>
2026-05-31 22:19:26 +0300

    parametrized testBase added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -25,11 +25,11 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.baseUrl         = System.getProperty("baseUrl", "https://demoqa.com");
-        Configuration.browser         = System.getProperty("browser", "chrome");
-        Configuration.browserVersion  = System.getProperty("browserVersion", "128.0");
-        Configuration.browserSize     = System.getProperty("browserSize", "1920x1080");
-        Configuration.headless        = Boolean.parseBoolean(System.getProperty("headless", "false"));
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "128.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
 
         String remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.remote = remote;
